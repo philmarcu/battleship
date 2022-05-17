26a2a9f0b041c18073d75045027fe8e72f22c9a8
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-17 16:18:33 -0600

    add notes for myself (SG)

--- lib/ship.rb
+++ lib/ship.rb
@@ -4,18 +4,19 @@ class Ship
               :length,
               :health,
               :sunk
-  def initialize(name, length)
-    @name = name
-    @length = length
-    @health = @length
+
+  def initialize(name, length) #ship type, ship length
+    @name = name #ship type
+    @length = length #ship attribute
+    @health = @length #health = how many increments
     @sunk = false
   end
 
   def sunk?
-    @health < 1
+    @health < 1 #if health = 0
   end
 
   def hit
-    @health -= 1
-  end
+    @health -= 1 #health = health - 1
+  end #do we want it to return the current health or nil?
 end
