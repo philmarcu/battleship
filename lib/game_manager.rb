2426814bdde6d635821d0c1c82a860f192f0ba5d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-23 13:30:50 -0600

    Progress with comp_place method

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -1,7 +1,9 @@
 class Game_Manager
+attr_reader :board
 
   def initialize
     welcome_message
+    @board = Board.new
   end
 
   def welcome_message
@@ -17,6 +19,24 @@ class Game_Manager
     else
       puts "Invalid entry, please use p or q"
     end
+  end
+
+  def comp_place
+#     rand_choice = Random.new
+#     rand_choice.rand(1..2) #chooses 1 or 2
+# #### ----
+#     if rand_choice == 1 #horizontal
+#       @board.cells.key.sample
+#     elsif rand_choice == 2 #vertical
+#
+#     end
+  ## FOR SUBMARINE
+  ##3 horizontal options per line, 3*4 = 12 horizontal options
+  ## 3 vertical options per line, 3*4 = 12 vertical options
+
+  
+
+
   end
 
   def run_game
--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -6,8 +6,11 @@ require './lib/game_manager'
 
 RSpec.describe Ship do
   game = Game_Manager.new
+  board = Board.new
 
   it 'exists' do
     expect(game).to be_a(Game_Manager)
+    expect(board).to be_a(Board)
   end
+
 end
